cf3c146fe214fc05b04aed90dab4d3a14e9e0778
Ariniziya <murchelot@yandex.ru>
2021-10-29 01:10:27 +0300

    class Lion isolated;
    let Alex lie here for now;

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,9 +5,9 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
-    Feline feline;
+    private Feline feline;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -15,11 +15,9 @@ public class Lion {
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
-    }
-
-    public Lion(Feline feline) {
         this.feline = feline;
     }
+
     public int getKittens() {
         return feline.getKittens();
     }
