3c301242ef6f2afe8d0895a5998d90460d3a7b11
Ariniziya <murchelot@yandex.ru>
2021-10-29 23:14:33 +0300

    ощущение, что всё сделал №6

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -2,56 +2,25 @@ package com.example;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
-
-@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
 
-    Feline testFeline = new Feline();
-    Lion testLion;
-    {
-        try {
-            testLion = new Lion("Самка", testFeline);
-        } catch (Exception lionTestException){
-            lionTestException.printStackTrace();
-        }
+    @Test
+    public void getKittensTest() throws Exception {
+        Feline testFeline = new Feline();
+        Lion testLion = new Lion("Самец", testFeline);
+        int actual = testLion.getKittens();
+        Assert.assertEquals(1, actual);
     }
 
     @Test
-    public void lionGetKittensWillReturnOneTest() {
-        int actualKittensCount = testFeline.getKittens();
-        int expectedKittenCount = 1;
-        assertEquals(expectedKittenCount, actualKittensCount);
-    }
-
-    @Test
-    public void lionGetFoodWillReturnMeatListTest() {
-        try {
-            List<String> actualFoodReturn = testLion.getFood();
-            List<String> expectedFoodReturn = List.of("Животные", "Птицы", "Рыба");
-            assertEquals(expectedFoodReturn, actualFoodReturn);
-        } catch (Exception animalFoodException) {
-            Assert.fail("Exeption " + animalFoodException);
-        }
-    }
-
-    @Mock
-    Feline mocFeline;
-
-    @Test
-    public void lionSexExceptionMessageCorrectTextTest() {
-        try {
-            Lion lionIncorrectSex = new Lion("Не мальчик, не девочка, а единорог", mocFeline);
-        } catch (Exception lionSexException) {
-            String actualExceptionText = lionSexException.getMessage();
-            String expectedExceptionText = "Используйте допустимые значения пола животного - самец или самка";
-            assertEquals(expectedExceptionText, actualExceptionText);
-        }
+    public void getFoodTest() throws Exception {
+        Feline testFeline = new Feline();
+        Lion testLion = new Lion("Самец", testFeline);
+        List<String> actual = testLion.getFood();
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        Assert.assertEquals(expected, actual);
     }
 }
