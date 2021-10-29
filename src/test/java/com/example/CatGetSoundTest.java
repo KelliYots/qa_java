3860ac419b398f21f845b576bb38e44c06dd20b7
Ariniziya <murchelot@yandex.ru>
2021-10-29 05:10:09 +0300

    cat get sound test

---
+++ src/test/java/com/example/CatGetSoundTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CatGetSoundTest {
+    Cat cat = new Cat(new Feline());
+
+    @Test
+    public void catGetSoundTest() {
+    String actualSound = cat.getSound();
+    assertEquals("Мяу", actualSound);
+    }
+}
