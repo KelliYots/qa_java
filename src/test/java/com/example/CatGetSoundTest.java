78b064214096b2a9373e6a76984e0ce4d5f478fd
Ariniziya <murchelot@yandex.ru>
2021-10-31 18:12:14 +0300

    Внедрён хотя бы один мок все тесты прогнаны после правок

--- src/test/java/com/example/CatGetSoundTest.java
+++ src/test/java/com/example/CatGetSoundTest.java
@@ -1,14 +1,18 @@
 package com.example;
 
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.junit.Assert.assertEquals;
 
 public class CatGetSoundTest {
-    Cat testCat = new Cat(new Feline());
+
+    @Mock
+    Feline testFeline;
 
     @Test
     public void catGetSoundTest() {
+        Cat testCat = new Cat(testFeline);
         String actualSound = testCat.getSound();
         assertEquals("Мяу", actualSound);
     }
